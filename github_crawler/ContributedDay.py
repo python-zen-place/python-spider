be48412950ac99c327c3e0f08934fc3888a2c964
Weilet <453546129@qq.com>
2019-10-05 23:51:11 +0800

    add language analyses

--- github_crawler/ContributedDay.py
+++ github_crawler/ContributedDay.py
@@ -2,9 +2,12 @@ from datetime import datetime
 
 
 class ContributedDay:
+    total_contributions = 0
+
     def __init__(self, date, contributes):
         self.date = datetime.strptime(date, '%Y-%m-%d')
         self.contributes = int(contributes)
+        self.total_contributions += int(contributes)
 
     def __repr__(self):
         if self.contributes == 1:
--- github_crawler/__init__.py
+++ github_crawler/__init__.py
@@ -1,7 +1,10 @@
+import multiprocessing
+
 from bs4 import BeautifulSoup
 import requests
 
 from .ContributedDay import ContributedDay
+from .Language import Language
 
 
 def require_login(fun):
@@ -10,50 +13,86 @@ def require_login(fun):
             return fun(self, *args, **kwargs)
         else:
             raise AssertionError('Require Login')
-
     return wrapper
+
+# warning: it seems that using class variable to record the total use of language may cause error when you try to
+#          get two or more user's language information and I can't think out a solution to it.
+#          so you should avoid using more than one instance at one time.
 
 
 class Crawler:
-    def __init__(self, username):
-        self.contribute_days = []
+    def __init__(self, username, password=None):
         self.username = username
-        self.total_contributes = 0
+        self.password = password
         self.is_login = False
+
+        self.contributed_days = []
         self.repositories_links_list = []
+        self.language = []
 
     @property
     def BASEURL(self):
         return 'https://github.com/'
 
     @property
-    def contribute_url(self):
+    def login_url(self):
+        return 'https://github.com/login'
+
+    @property
+    def contribution_url(self):
         return f'https://github.com/users/{self.username}/contributions'
 
     @property
     def repositories_url(self):
         return f'https://github.com/{self.username}?tab=repositories'
 
-    def get_contribute(self):
-        res = requests.get(self.contribute_url).text
+    def login(self):
+        assert self.password, 'Password required'
+
+    def get_contributions(self):
+        res = requests.get(self.contribution_url).text
         soup = BeautifulSoup(res, 'html.parser')
-        self.total_contributes = int(soup.find('h2').string.split()[0])
         rects = soup.find_all('rect')
         for rect in rects:
             if rect['fill'] == '#ebedf0':
                 pass
             else:
-                self.contribute_days.append(ContributedDay(rect['data-date'], rect['data-count']))
+                self.contributed_days.append(ContributedDay(rect['data-date'], rect['data-count']))
+
+    def show_contributions(self):
+        if len(self.contributed_days) == 0:
+            print('It seems that you have not worked this year')
+        else:
+            print(f'You contributions {ContributedDay.total_contributions} times in the last year\n' +
+                  '\n'.join(map(str, self.contributed_days)))
 
     def get_repositories(self):
         res = requests.get(self.repositories_url).text
         soup = BeautifulSoup(res, 'html.parser')
-        self.repositories_links_list = [self.BASEURL+x['href'] for x in soup.find_all('a', {
-            'itemprop': 'name codeRepository'
-        })]
+        self.repositories_links_list = [self.BASEURL + tag['href']
+                                        for tag in soup.find_all('a', itemprop='name codeRepository')]
 
-    def __repr__(self):
-        if len(self.contribute_days) == 0:
-            return 'It seems that you have not worked this year'
-        return f'You contributions {self.total_contributes} times in the last year\n' +\
-               '\n'.join(map(str, self.contribute_days))
+    def show_repositories(self):
+        if len(self.repositories_links_list) == 0:
+            return 'It seems that you have no public repository'
+        else:
+            print(f'You have  {len(self.repositories_links_list)} repository\n' +
+                  '\n'.join(self.repositories_links_list))
+
+    def get_language(self):
+        for link in self.repositories_links_list:
+            res = requests.get(link).text
+            soup = BeautifulSoup(res, 'html.parser')
+            for tag in soup.find_all('span', class_='language-color', itemprop='keywords'):
+                language = tag['aria-label'].split()[0]
+                use = float(tag['aria-label'].split()[1][:-1])/100
+                self.language.append(Language(language, use))
+
+    def show_language(self):
+        if Language.total_use == 0:
+            print('It seems that you have not worked this year')
+        else:
+            print(f'You use {len(Language.language_dict.keys())} types of language \n' +\
+                  '\n'.join(map(str, self.language)))
+
+
