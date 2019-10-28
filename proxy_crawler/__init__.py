641c9624868cb0f21971b9b8a2cd3f6bae743b82
Weilet <453546129@qq.com>
2019-10-28 21:09:22 +0800

    make the proxy_crawler faster

--- proxy_crawler/__init__.py
+++ proxy_crawler/__init__.py
@@ -1,53 +1,58 @@
-from bs4 import BeautifulSoup
-import re
-import requests
-from config import config
-from multiprocessing import Pool
 import os
+import re
 import time
 
+from bs4 import BeautifulSoup
+import aiohttp
+import asyncio
+from multiprocessing import Pool
 
-def get_proxy(url):
-    headers = {
-        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                      "Chrome/78.0.3904.70 Safari/537.36",
-    }
-    res = requests.get(url, headers=headers).text
-    soup = BeautifulSoup(res, 'html.parser')
+from config import config
+from error import IPBannedException
+
+headers = {
+    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/78.0.3904.70 Safari/537.36",
+}
+
+
+async def get_proxy(url, session):
+    res = await session.get(url, headers=headers)
+    html = await res.text()
+    return html
+
+
+async def test_proxy(proxy, session, test_url='http://www.baidu.com'):
+    try:
+        await session.get(test_url, proxy='http://' + proxy, timeout=1)
+        return proxy
+    except (asyncio.TimeoutError, aiohttp.client_exceptions.ClientOSError) as why:
+        return None
+
+
+def parse(html):
+    soup = BeautifulSoup(html, 'html.parser')
     servers = soup.find_all(string=re.compile(r'(\d{1,3}\.){3}\d{1,3}'))
     ports = [port.string for port in soup.find_all(name='td', string=re.compile(r'^\d{1,5}$'))]
     return ['{}:{}'.format(server, port) for server, port in zip(servers, ports)]
 
 
-def test_proxy(proxy, test_url='http://www.baidu.com'):
-    proxies = {
-        'http': proxy,
-        'https': proxy
-    }
-    try:
-        res = requests.get(test_url, proxies=proxies, timeout=1)
-        return proxy
-    except (requests.exceptions.ProxyError, requests.exceptions.Timeout) as e:
-        return None
+async def main(loop):
+    with Pool(os.cpu_count()) as pool:
+        async with aiohttp.ClientSession() as session:
+            get_proxy_tasks = [loop.create_task(get_proxy(url, session)) for url in config['urls']]
+            finished, unfinished = await asyncio.wait(get_proxy_tasks)
+            pages = [task.result() for task in finished]
+
+            parse_tasks = [pool.apply_async(parse, args=(page,)) for page in pages]
+            untested_proxies = [task.get() for task in parse_tasks]
+
+            test_proxy_tasks = [loop.create_task(test_proxy(proxy, session)) for each in untested_proxies for proxy in
+                                each]
+            finished, unfinished = await asyncio.wait(test_proxy_tasks)
+            available_proxies = [task.result() for task in finished if task.result()]
+            if not available_proxies:
+                raise IPBannedException
+    return available_proxies
 
 
-if __name__ == '__main__':
-    urls = config.get('urls')
-    start = time.time()
-    res, proxies = [], []
-    pool = Pool(os.cpu_count())
-    tasks = [pool.apply_async(get_proxy, args=(url,)) for url in urls]
-    pool.close()
-    pool.join()
-    pool = Pool(os.cpu_count(logical=False))
-    for each_list in [task.get() for task in tasks]:
-        proxies.extend(each_list)
-
-    tasks = [pool.apply_async(test_proxy, args=(proxy,)) for proxy in proxies]
-    pool.close()
-    pool.join()
-    for each_list in [task.get() for task in tasks]:
-        res.extend(each_list)
-
-    print('time: {}'.format(time.time() - start))
-    print(res)
---
+++ proxy_crawler/error.py
@@ -0,0 +1,2 @@
+class IPBannedException(BaseException):
+    pass
